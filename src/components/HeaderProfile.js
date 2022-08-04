3b2285f3cc8e161c59e5da9c1eda70056766b12c
pridantaraka <pridantaraka@gmail.com>
2022-08-04 15:52:53 +0700

    update-nav-profile

--- src/components/HeaderProfile.js
+++ src/components/HeaderProfile.js
@@ -1,40 +1,44 @@
-import { Button, Nav, Form } from 'react-bootstrap'
+import { Nav, Form } from 'react-bootstrap'
 import { VscFilterFilled } from "react-icons/vsc";
-import { CgShoppingCart } from "react-icons/cg";
+import { CgMail, CgBell, CgShoppingCart, CgProfile } from "react-icons/cg";
 
 import logo1 from '../assets/images/shopping-bag1.png'
 
-export default function HeaderHome() {
+export default function HeaderProfile() {
     return(
         <>
             <header className='mw-100 nav-home-height py-3 d-flex justify-content-around align-items-center'>
                 <Nav defaultActiveKey="/home" as="ul" className='gap-3'>
                     <Nav.Item as="li" className='logo-color'>
                         <Nav.Link href="/" className='d-flex flex-row'>
-                        <div><img src={logo1} alt='Logo' className='fluid align-self-center'/></div>
+                        <div className=''><img src={logo1} alt='Logo' className='fluid align-self-center'/></div>
                         <h4 className='align-self-center mb-0'>Negozio</h4>
                         </Nav.Link>
                     </Nav.Item>
                     <Nav.Item as="li" className='d-flex flex-row align-items-center logo-color'>
-                        <Nav.Link eventKey="link-1" >
+                        <Nav.Link eventKey="link-1">
                             <Form>
                                 <Form.Control size='small' placeholder='Search' type='search' className='nav-search-box'>
                                 
                                 </Form.Control>
                             </Form>
                         </Nav.Link>
-                        <Nav.Link href="/profile"><div className='logo-color'><VscFilterFilled size={24} /></div></Nav.Link>
+                        <Nav.Link eventKey="link-1"><div className='logo-color'><VscFilterFilled size={24} /></div></Nav.Link>
                     </Nav.Item>
                     <Nav.Item as="li">
+                        
                     </Nav.Item>
                     <Nav.Item as="li" className='d-flex flex-row align-items-center'>
-                        <Nav.Link eventKey="link-1"><div className='align-self-center logo-color px-0'><CgShoppingCart size={24} /></div></Nav.Link>
+                        <Nav.Link eventKey="link-1"><div className='align-self-center logo-color'><CgShoppingCart size={24}/></div></Nav.Link>
                     </Nav.Item>
                     <Nav.Item as="li" className='d-flex flex-row align-items-center'>
-                        <Button className='nav-button'>Login</Button>
+                        <Nav.Link eventKey="link-1"><div className='align-self-center logo-color'><CgBell size={24} /></div></Nav.Link>
                     </Nav.Item>
                     <Nav.Item as="li" className='d-flex flex-row align-items-center'>
-                        <Button className='nav-button'>SignUp</Button>
+                        <Nav.Link eventKey="link-1"><div className='align-self-center logo-color'><CgMail size={24} /></div></Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item as="li" className='d-flex flex-row align-items-center'>
+                        <Nav.Link eventKey="link-1"><div className='align-self-center logo-color'><CgProfile size={24}/></div></Nav.Link>
                     </Nav.Item>
                 </Nav>
             </header>
