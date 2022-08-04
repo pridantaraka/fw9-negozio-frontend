fc522698383dc93b4aea0491b1f684f0c25b5054
prasetyoow <praztamer@gmail.com>
2022-08-04 20:37:14 +0700

    add routing for auth pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
-
 import Home from './pages/home/Home'
-import Login from './auth/Login'
+import Login from './pages/auth/Login'
+import LoginSeller from './pages/auth/LoginSeller'
+import Register from './pages/auth/Register'
+import RegisterSeller from './pages/auth/RegisterSeller'
+import ResetPassword from './pages/auth/ResetPassword'
+import ConfirmResetPassword from './pages/auth/ConfirmResetPassword'
 
 function App() {
     return(
         <>
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="login" element={<Login />}/>
+            <Route path="login" element={<Login />} />
+            <Route path="loginseller" element={<LoginSeller />} />
+            <Route path="register" element={<Register />} />
+            <Route path="registerseller" element={<RegisterSeller />} />
+            <Route path="resetpassword" element={<ResetPassword />} />
+            <Route path="confirmresetpassword" element={<ConfirmResetPassword />} />
         </Routes>
         </>
     )
