34519aa1d3e9787c12c101f640157e158b07add2
pridantaraka <pridantaraka@gmail.com>
2022-08-04 11:26:35 +0700

    pindah folder auth di pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 
 import Home from './pages/home/Home'
-import Login from './auth/Login'
+import Login from './pages/auth/Login'
 
 function App() {
     return(
